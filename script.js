71d8bfccfece720c113b2af28c9509ba6be7be26
Rifavp <iamrifuz@gmail.com>
2025-02-02 02:50:20 +0530

    add right side result page

---
+++ script.js
@@ -0,0 +1,4 @@
+// Functionality for the "Try Another" button
+document.getElementById("tryAnotherBtn").addEventListener("click", function () {
+  alert("Try another journey!"); // Replace this with your desired functionality
+});
